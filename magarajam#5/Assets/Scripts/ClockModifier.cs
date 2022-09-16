82df2fe6bbe8be7271d654faa81fd5c4e6acdef0
Kadir Cem Yılmaz <kadircemyilmaz@ogrenci.beykoz.edu.tr>
2022-09-16 05:35:41 +0300

    Timer with Clock

---
+++ magarajam#5/Assets/Scripts/ClockModifier.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockModifier : MonoBehaviour
+{
+    private Transform clockHandTransform;
+    Timer timer;
+    private float ClockTime;
+
+    public AudioClip impact;
+    AudioSource audioSource;
+
+    public bool ringPlay;
+    void Start()
+    {
+
+        timer = GameObject.Find("Timer").GetComponent<Timer>();
+        clockHandTransform = transform.Find("ClockHandler");
+        ClockTime = timer.timerLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timer.timerLeft != 0)
+        {
+            clockHandTransform.Rotate(Vector3.forward * 240 / -ClockTime * Time.deltaTime);
+        }
+
+        
+    }
+    private void FixedUpdate()
+    {
+        if (timer.dayEnd)
+        {
+            PowerSound();
+            timer.dayEnd = false;
+        }
+    }
+    void PowerSound()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(impact, 0.2F);
+
+    }
+
+
+}
