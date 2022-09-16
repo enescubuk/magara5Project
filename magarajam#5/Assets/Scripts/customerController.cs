377c9f28149b61d89039d6aaca583afee708c3e4
Enes <enescubuk10@gmail.com>
2022-09-16 17:15:35 +0300

    the rock

--- magarajam#5/Assets/Scripts/customerController.cs
+++ magarajam#5/Assets/Scripts/customerController.cs
@@ -5,28 +5,72 @@ using UnityEngine.UI;
 
 public class customerController : MonoBehaviour
 {
-    public Sprite[] allCustomers;
+    public Text customerSaying;
+    
+    public GameObject marketElements;
+    public Image[] allCustomers;
+
+    public string[] normalTexts;
+    public string[] susTexts;
     private bool isBusy = false;
+    string customerName;
+    
+    int detectCustomer;
     // Start is called before the first frame update
+    private void Awake()  
+    {
+        //customerSaying = GameObject.Find("customerSaying").GetComponent<Text>();
+    }
     void Start()
     {
-        
+        randomCustomer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isBusy == false)
-        {
-            randomCustomer();
-        }
+        
     }
     void randomCustomer()
     {
         isBusy = true;
         int a = Random.Range(0,allCustomers.Length);
         Debug.Log(a);
-        Debug.Log(allCustomers[a].name);
+        allCustomers[a].enabled = true;
+        marketElements.SetActive(true);
+        customerName = allCustomers[a].name;
+        detection();
+    }
+    void detection()
+    {
+        if (customerName.Contains("normal"))
+        {
+            detectCustomer = 0;
+        }
+        else if (customerName.Contains("sus"))
+        {
+            detectCustomer = 1;
+        }
+        whatCustomerSay();
+    }
+    
+    void whatCustomerSay()
+    {
+        switch (detectCustomer)
+        {
+            case 0: 
+                Debug.Log("normal");
+                customerSaying.text = normalTexts[Random.Range(0,normalTexts.Length)];
+                    break;
+            case 1: 
+                Debug.Log("sus");
+                customerSaying.text = susTexts[Random.Range(0,susTexts.Length)];
+                    break;
+        }
+    }
+    public void toProduction()
+    {
+        marketElements.SetActive(false);
     }
     void moveCustomerIn()
     {
@@ -36,6 +80,7 @@ public class customerController : MonoBehaviour
     {
         //b konumundan a konumuna git
         isBusy = false;
+        randomCustomer();
     }
     void defaultBuy()
     {
